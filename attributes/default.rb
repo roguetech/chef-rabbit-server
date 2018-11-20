5080a038189e07ba71828b3053e6c7d93a372009
Patrick Roughan <patrickroughan@realexpayments.com>
2018-11-20 15:23:59 +0000

    adding attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,4 @@ node.default['rabbitmq']['default_user'] = 'guest'
 node.default['rabbitmq']['default_pass'] = 'guest'
 node.default['rabbitmq']['loopback_users'] = nil
 node.default['rabbitmq']['management']['load_definitions'] = true
+node.default['rabbitmq']['enabled_plugins']
