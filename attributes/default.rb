64fd1e54f870fc558dfed78a742fa1ab8d2aad42
Patrick Roughan <patrickroughan@realexpayments.com>
2018-11-20 11:59:48 +0000

    Changing attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-node['erlang']['install_method'] = "esl"
-node['rabbitmq']['version'] = "3.7.3"
-node['rabbitmq']['default_user'] = 'guest'
-node['rabbitmq']['default_pass'] = 'guest'
-node['rabbitmq']['loopback_users'] = nil
+node.default['erlang']['install_method'] = "esl"
+node.default['rabbitmq']['version'] = "3.7.3"
+node.default['rabbitmq']['default_user'] = 'guest'
+node.default['rabbitmq']['default_pass'] = 'guest'
+node.default['rabbitmq']['loopback_users'] = nil
