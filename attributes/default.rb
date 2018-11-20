f29b824a71311b7eb87c59289948d7284987b5c1
Patrick Roughan <patrickroughan@realexpayments.com>
2018-11-20 15:37:57 +0000

    adding attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['erlang']['install_method'] = "esl"
 default['rabbitmq']['version'] = "3.7.3"
 default['rabbitmq']['default_user'] = 'guest'
 default['rabbitmq']['default_pass'] = 'guest'
-sdefault['rabbitmq']['loopback_users'] = nil
+default['rabbitmq']['loopback_users'] = nil
