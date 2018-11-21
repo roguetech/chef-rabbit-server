3cac11b4afda69ef7da5aa5361f2c8583445b332
Patrick Roughan <patrickroughan@realexpayments.com>
2018-11-21 11:55:51 +0000

    users

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,16 +1,14 @@
 default['erlang']['install_method'] = "esl"
 default['rabbitmq']['version'] = "3.7.9"
-#default['rabbitmq']['default_user'] = 'guest'
-#default['rabbitmq']['default_pass'] = 'guest'
+default['rabbitmq']['default_user'] = 'guest'
+default['rabbitmq']['default_pass'] = 'guest'
 default['rabbitmq']['loopback_users'] = nil
-default['rabbitmq']['user'] = 'administrator'
-default['rabbitmq']['pass'] = 'admin'
 
 # Plugins
 default['rabbitmq']['enabled_plugins'] = ['rabbitmq_management_visualiser','rabbitmq_test','amqp_client','cowboy','eldap','mochiweb','sockjs','webmachine']
 
 # Create users
-default['rabbitmq']['enable_users'] = [
+default['rabbitmq']['enabled_users'] = [
   {
     :name => 'administrator',
     :password => 'admin',
