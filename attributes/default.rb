87f259a71592c665d4941ffebb595472f3ed4c9e
Patrick Roughan <patrickroughan@realexpayments.com>
2018-11-21 11:31:04 +0000

    adding users

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default['rabbitmq']['version'] = "3.7.9"
 default['rabbitmq']['default_user'] = 'guest'
 default['rabbitmq']['default_pass'] = 'guest'
 default['rabbitmq']['loopback_users'] = nil
-default['rabbitmq']['user'] = 'admin'
+default['rabbitmq']['user'] = 'administrator'
 default['rabbitmq']['pass'] = 'admin'
 
 # Plugins
